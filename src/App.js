98f03bbac02bf4defbf03c78498534c88976c021
housemax77 <63367971+housemax77@users.noreply.github.com>
2021-04-19 18:25:42 -0500

    Added all new JSX

---
+++ src/App.js
@@ -0,0 +1,45 @@
+export function App() {
+  return (
+    <>
+      <h1 aria-label="Page Heading" id="heading">
+        To Do List
+      </h1>
+      <form id="form">
+        <input placeholder="To Do" id="toDo" />
+        <input aria-label="To Do Time" type="time" id="time" />
+        <input id="to-do-submit-button" type="submit" value="Add To Do" />
+      </form>
+      <div class="dropdown">
+        <button class="dropbtn">Dropdown</button>
+        <div
+          aria-label="Sort By Time Or To Do Dropdown"
+          class="dropdown-content"
+        >
+          <a
+            aria-label="Sort By Time"
+            id="timeSortButton-"
+            onclick="callSortTimes()"
+          >
+            Sort By Time
+          </a>
+          <a
+            aria-label="Sort Alphabeticlly"
+            id="alphabeticalSortButton-"
+            onclick="callSortTimes()"
+          >
+            Sort Alphabeticlly
+          </a>
+        </div>
+      </div>
+      <input
+        aria-label="Text To Search To Do"
+        type="text"
+        id="searchToDo"
+        placeholder="🔍 Search"
+        onkeyup="toDoSort()"
+      />
+      <div id="sortBy">Not Sorting</div>
+      <ol id="List"></ol>
+    </>
+  );
+}
