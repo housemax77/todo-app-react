ce4da96413ecadb50c6897f35fcb7639ba1a95bb
housemax77 <63367971+housemax77@users.noreply.github.com>
2021-05-22 21:30:06 -0500

    removed unneeded commented toDo creation

--- src/App.js
+++ src/App.js
@@ -23,33 +23,6 @@ export function App() {
   return (
     <>
       <Header />
-      {/* <form
-        onSubmit={(event) => {
-          event.preventDefault();
-          setToDos([
-            ...toDos,
-            { toDo: toDo, time: time, done: false, editing: false },
-          ]);
-          setToDo("");
-          setTime("");
-        }}
-      >
-        <input
-          placeholder="To Do"
-          value={toDo}
-          onChange={(event) => setToDo(event.target.value)}
-          className="toDo"
-        />
-        <input
-          aria-label="To Do Time"
-          type="time"
-          value={time}
-          onChange={(event) => setTime(event.target.value)}
-          className="time"
-        />
-        <input className="submit-toDo" type="submit" value="Add To Do" />
-      </form> */}
-
       <CreateToDo
         time={time}
         setTime={setTime}
