0fa5902ee70d352da56a6582b4cb1f8f3fbdf933
housemax77 <63367971+housemax77@users.noreply.github.com>
2021-05-22 21:30:32 -0500

    Revert "Create to do component"

--- src/CreateToDo.js
+++ src/CreateToDo.js
@@ -1,38 +1,35 @@
-import React from "react";
+// import React from "react";
 
-export function CreateToDo(props) {
-  const time = props.time;
-  const toDo = props.toDo;
-  const setToDo = props.setToDo;
-  const setToDos = props.setToDos;
-  const setTime = props.setTime;
-  const toDos = props.toDos;
-  return (
-    <form
-      onSubmit={(event) => {
-        event.preventDefault();
-        setToDos([
-          ...toDos,
-          { toDo: toDo, time: time, done: false, editing: false },
-        ]);
-        setToDo("");
-        setTime("");
-      }}
-    >
-      <input
-        placeholder="To Do"
-        value={toDo}
-        onChange={(event) => setToDo(event.target.value)}
-        className="toDo"
-      />
-      <input
-        aria-label="To Do Time"
-        type="time"
-        value={time}
-        onChange={(event) => setTime(event.target.value)}
-        className="time"
-      />
-      <input className="submit-toDo" type="submit" value="Add To Do" />
-    </form>
-  );
-}
+// export function CreateToDo(props) {
+//   const setToDo = props.setToDo;
+//   const setToDos = props.setToDos;
+//   const setTime = props.setTime;
+//   return (
+//     <form
+//       onSubmit={(event) => {
+//         event.preventDefault();
+//         setToDos([
+//           ...toDos,
+//           { toDo: toDo, time: time, done: false, editing: false },
+//         ]);
+//         setToDo("");
+//         setTime("");
+//       }}
+//     >
+//       <input
+//         placeholder="To Do"
+//         value={toDo}
+//         onChange={(event) => setToDo(event.target.value)}
+//         className="toDo"
+//       />
+//       <input
+//         aria-label="To Do Time"
+//         type="time"
+//         value={time}
+//         onChange={(event) => setTime(event.target.value)}
+//         className="time"
+//       />
+//       <input className="submit-toDo" type="submit" value="Add To Do" />
+//     </form>
+//   );
+// }
